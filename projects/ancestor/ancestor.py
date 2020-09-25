c787d53f3fdc69156a6e4249b2935529c484ad1b
Ian <lolsugotpwnd@yahoo.com>
2020-09-24 19:46:49 -0500

    ancestor done

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,29 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+def getParents(ancestors, node):
+    parents = []
+    for pair in ancestors:
+        if pair[1] == node:
+            parents.append(pair[0])
+    return parents
+
+
+def dft_recursive(ancestors, node, distance):
+    parents = getParents(ancestors, node)
+
+    aged_one = (node, distance)
+
+    for parent in parents: 
+        pair = dft_recursive(ancestors, parent, distance + 1)
+        if pair[1] > aged_one[1]:
+            aged_one = pair
+    
+    return aged_one
+
+
+def earliest_ancestor(ancestors, starting_node, distance=0):
+    aged_one = dft_recursive(ancestors, starting_node, distance)
+
+    if aged_one[0] == starting_node:
+        return -1
+
+    return aged_one[0]
